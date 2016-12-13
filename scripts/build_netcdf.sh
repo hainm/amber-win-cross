5056fedcfd810afd46b7d9a5f6c307e4ee5e9369
Hai Nguyen <hainm.comp@gmail.com>
2016-12-13 14:51:12 -0500

    fix PREFIX

--- scripts/build_netcdf.sh
+++ scripts/build_netcdf.sh
@@ -4,5 +4,5 @@ yum install unzip -y
 curl -fsS -o netcdf-4.3.3.zip ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.3.zip
 unzip netcdf-4.3.3.zip
 cd netcdf-4.3.3
-./configure --prefix=`pwd`/local --enable-static --disable-netcdf-4 --prefix=/usr/local/ --disable-dap --host=x86_64-w64-mingw32
+./configure --prefix=`pwd`/local --enable-static --disable-netcdf-4 --disable-dap --host=x86_64-w64-mingw32
 make -r install
