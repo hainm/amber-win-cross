170c33f671003e7b09ee9a2f95965db03a207f8d
Hai Nguyen <hainm.comp@gmail.com>
2016-12-13 14:04:59 -0500

    local

--- scripts/install_netcdf.sh
+++ scripts/install_netcdf.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+mkdir local/
 yum install unzip -y
 curl -fsS -o netcdf-4.3.3.zip ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.3.zip
 unzip netcdf-4.3.3.zip
 cd netcdf-4.3.3
-./configure --enable-static --disable-netcdf-4 --prefix=/usr/local/ --disable-dap --host=x86_64-w64-mingw32
+./configure --prefix=`pwd`/local --enable-static --disable-netcdf-4 --prefix=/usr/local/ --disable-dap --host=x86_64-w64-mingw32
 make -r install
