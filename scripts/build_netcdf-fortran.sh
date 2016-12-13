8e811ebdacd0d1e231ccd58dbc32eabacf5df342
Hai Nguyen <hainm.comp@gmail.com>
2016-12-13 17:16:50 -0500

    hey there

--- scripts/build_netcdf-fortran.sh
+++ scripts/build_netcdf-fortran.sh
@@ -5,12 +5,13 @@ version=4.4.4
 wget https://github.com/Unidata/netcdf-fortran/archive/v$version.tar.gz -O netcdf-fortran-$version.tar.gz
 tar -xf netcdf-fortran-$version.tar.gz
 PREFIX=/work/local/
+echo "PREFIX" $PREFIX
 
 cd netcdf-fortran-$version
 
 # below is lightly changed from
 # https://github.com/conda-forge/netcdf-fortran-feedstock/blob/master/recipe/build.sh
-texport LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 export CFLAGS="$CFLAGS fPIC -I$PREFIX/include"
 
