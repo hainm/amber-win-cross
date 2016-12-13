e9e840c62ad5122920a41616d33266161f46f7ef
Hai Nguyen <hainm.comp@gmail.com>
2016-12-13 18:26:10 -0500

    holly crap

--- scripts/build_netcdf-fortran.sh
+++ scripts/build_netcdf-fortran.sh
@@ -13,15 +13,10 @@ echo "SRC_DIR" ${SRC_DIR}
 
 export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
 
-# Build static.
-mkdir build_static && cd build_static
 ./configure --prefix=$PREFIX \
             --enable-static \
             CPPFLAGS=-I/work/local/include \
             --host=x86_64-w64-mingw3
-
 make
-# ctest  # Run only for the shared lib build to save time.
 make install
-
 cd ../../
