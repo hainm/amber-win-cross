3f44d2cb8652d037cc96bb6a6d5e516bc3d18ec9
Hai Nguyen <hainm.comp@gmail.com>
2016-12-13 19:26:52 -0500

    use 4.4.*

--- scripts/build_netcdf-fortran.sh
+++ scripts/build_netcdf-fortran.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-yum install -y cmake
 version=4.4.4
 wget https://github.com/Unidata/netcdf-fortran/archive/v$version.tar.gz -O netcdf-fortran-$version.tar.gz
 tar -xf netcdf-fortran-$version.tar.gz
@@ -16,12 +15,13 @@ export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
 # Build static.
 mkdir build_static && cd build_static
 ./configure --prefix=$PREFIX \
-            --enable-static \
+            --diable-shared \
             CPPFLAGS=-I/work/local/include \
+            LDFLAGS=-L/work/local/lib -lnetcdf \
             --host=x86_64-w64-mingw3
 
 make
-# ctest  # Run only for the shared lib build to save time.
 make install
+make test
 
 cd ../../
--- scripts/build_netcdf.sh
+++ scripts/build_netcdf.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 PREFIX=/work/local/
+version=4.4.4.1
 yum install unzip -y
-curl -fsS -o netcdf-4.3.3.zip ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.3.zip
-unzip netcdf-4.3.3.zip
-cd netcdf-4.3.3
+curl -fsS -o netcdf-$version.zip ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-$version.zip
+unzip netcdf-$version.zip
+cd netcdf-$version
 ./configure --prefix=$PREFIX --enable-static --disable-netcdf-4 --disable-dap --host=x86_64-w64-mingw32
 make -r install
+make test
